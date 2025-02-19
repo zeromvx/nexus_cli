fabfcb700f70c48fdc634e05cf74adfbd750a7de
Sergey Karmanov <47343730+zeromvx@users.noreply.github.com>
2025-02-20 01:31:01 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Тг канал - https://t.me/zeromvx_crypto"
+echo "======================================="
+sleep 10
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y build-essential pkg-config libssl-dev git-all unzip
+
+apt install screen
+
+source "$HOME/.cargo/env"
+
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
+
+rustup update
+
+sudo apt remove -y protobuf-compiler
+
+curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v25.2/protoc-25.2-linux-x86_64.zip
+
+unzip protoc-25.2-linux-x86_64.zip -d "$HOME/.local"
+
+rm protoc-25.2-linux-x86_64.zip
+
+export PATH="$HOME/.local/bin:$PATH"
+
+screen -S nexus
+
+curl https://cli.nexus.xyz/ | sh
