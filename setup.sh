7b9bb0c9c43a0f676c6562819cf4fbb98df26693
Sergey Karmanov <47343730+zeromvx@users.noreply.github.com>
2025-02-20 10:39:41 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,6 +27,12 @@ unzip protoc-25.2-linux-x86_64.zip -d "$HOME/.local"
 
 sudo rm protoc-25.2-linux-x86_64.zip
 
-sudo echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+sudo swapoff -a
 
-protoc --version
+sudo rm -f /swapfile
+
+sudo dd if=/dev/zero of=/swapfile bs=1M count=8192
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
